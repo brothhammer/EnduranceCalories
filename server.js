da2293ef0d49ef8731ed7e39d30d83fd044d3ecb
Lauren Cowen <lauren.cowen@gmail.com>
2017-11-29 21:06:59 -0600

    trying something

--- server.js
+++ server.js
@@ -39,8 +39,11 @@ require("./routes/api.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-});
+// db.sequelize.sync({ force: true }).then(function() {
+//   app.listen(PORT, function() {
+//   	if(error) throw error;
+//     console.log("App listening on PORT " + PORT);
+//   });
+// });
+
+app.listen(PORT);
